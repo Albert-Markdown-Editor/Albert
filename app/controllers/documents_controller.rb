d577700699ce26677b320a85d623d91b6b2c1715
Alberto Hernandez Ceezo <AlbertoHdezCerezo@users.noreply.github.com>
2022-09-25 19:15:47 +0200

    Basic routing for documents configured

---
+++ app/controllers/documents_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class DocumentsController < ApplicationController
+  include DocumentSetters
+
+  before_action :set_document_with_slug, only: [:show]
+
+  # GET /documents
+  def index
+    @documents = Document.all
+  end
+
+  # GET /documents/<document_name>
+  # GET /documents/<project_name>/<document_name>
+  def show
+  end
+end
