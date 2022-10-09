a6d692be0414cd5e74d1bafebe3c41bba14a79c3
Alberto Hernandez Ceezo <AlbertoHdezCerezo@users.noreply.github.com>
2022-10-09 09:59:20 +0200

    Fix document fixtures and added test coverage for model

--- test/factories/document.rb
+++ test/factories/document.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   # Base Factory
   factory :document do
-    name { "Post about something" }
-    description { "This is a random post about something" }
+    sequence :name do |n| "Document n°#{n}" end
+    sequence :description do |n| "This is the description for document n°#{n}" end
     status { 0 }
   end
 end
--- test/models/document_test.rb
+++ test/models/document_test.rb
@@ -1,6 +1,22 @@
 require "test_helper"
 
 class DocumentTest < ActiveSupport::TestCase
+  # Model Slug Configuration
+  # ------------------------
+  test "A document uses its name as slug" do
+    model_with_friendly_id_sets_proper_slug? FactoryBot.create(:document), :name
+  end
+
+  test "If a document changes its name, its slug is updated according to the new name" do
+    model = FactoryBot.create(:document)
+    new_name = "New #{model.name}"
+    new_slug = new_name.parameterize
+    update_changes_slug = { name: new_name }
+    update_not_changes_slug = { description: "Again: #{model.description}" }
+
+    model_with_friendly_id_updates_slug_when_required? model, new_slug, update_changes_slug, update_not_changes_slug
+  end
+  
   # Model Validations
   # -----------------
   test "A document requires a name" do
