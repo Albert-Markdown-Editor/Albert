c43c0362eb0ffc3c9020aebced6b52f9f1fed3e2
Alberto Hernandez Ceezo <AlbertoHdezCerezo@users.noreply.github.com>
2023-01-20 21:37:21 +0100

    chore: rename name to title for BlogPost & Book

--- db/migrate/20230120072249_create_books.rb
+++ db/migrate/20230120072249_create_books.rb
@@ -1,7 +1,7 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books, id: :uuid do |t|
-      t.string :name
+      t.string :title
       t.string :summary
       t.date :release_date
       t.references :project, null: true, foreign_key: true, type: :uuid
