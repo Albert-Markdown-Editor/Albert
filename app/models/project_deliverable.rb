0f0c5c3a7df52bce7095adca31382a8052d2a867
Alberto Hernandez Ceezo <AlbertoHdezCerezo@users.noreply.github.com>
2023-01-20 22:19:07 +0100

    progress: view based kinds enum compulsory

--- app/models/project_deliverable.rb
+++ app/models/project_deliverable.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class ProjectDeliverable < ApplicationRecord
-  include ViewBasedModel
-
   # Ensures only deliverables with valid kinds are created
   enum kind: %w[Book BlogPost]
+
+  include ViewBasedModel
 end
