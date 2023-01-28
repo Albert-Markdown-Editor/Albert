54f97c53417d203b2f232d3ae9280dc6fdb7b28c
Alberto Hernandez Ceezo <AlbertoHdezCerezo@users.noreply.github.com>
2023-01-28 22:20:50 +0100

    Progress: multistep form model concern

---
+++ test/models/concerns/multistep_form_model_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+class MultistepFormModelTest < ActiveSupport::TestCase
+  class ValidMultistepFormMode
+    include ActiveModel::Model
+    include MultistepFormModel
+  end
+
+  def setup
+    @model = ValidMultistepFormMode.new
+  end
+
+  # Validations
+  # -----------
+  test "MultistepFormModel does not apply step validation if total and current attibutes are not specified" do
+    assert_nil @model.current_step
+    assert_nil @model.total_steps
+    assert @model.valid?
+  end
+
+  test "MultistepFormModel is valid only when total and current steps are the same" do
+    @model.current_step = 1
+    @model.total_steps = 2
+    refute @model.valid?
+
+    @model.current_step = 3
+    @model.total_steps = 3
+    assert @model.valid?
+  end
+end
