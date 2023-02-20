770a411f0c5730897b5f468b267171c708962d6e
Alberto Hernandez Ceezo <AlbertoHdezCerezo@users.noreply.github.com>
2023-02-20 17:34:31 +0100

    progress: minor UI adjustment

--- app/components/blogs/index_component.rb
+++ app/components/blogs/index_component.rb
@@ -20,8 +20,8 @@ module Blogs
           page_content.body do
             render(Common::Layout::PageContentComponent.new(title:, subtitle:, back_url:, back_label:)) do |component|
               component.with_right_content do
-                tag.div(class: "px-4 py-2 bg-gray-600 hover:bg-gray-800 text-white cursor-pointer") do
-                  tag.p("Create new Blog")
+                tag.div(class: "px-6 py-3 bg-gray-600 hover:bg-gray-800 transition-colors duration-400 text-white cursor-pointer") do
+                  tag.p("Create new Blog", class: "text-sm")
                 end
               end
 
